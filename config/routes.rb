9ca167bf408e7a346e4ac551930e60da59514ac8
FUMIGear <iso.fumi429@gmail.com>
2024-03-28 08:01:21 +0900

    3.4.4章の演習が終わったので、コミットします。 ルーティングの追加とそのテストを追加してます。

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get 'static_pages/about' # リスト3.19
   get  "static_pages/contact" # 演習3.4.3
   # root "application#hello" # リスト3.5：デプロイするため
-  root "static_pages#home" # リスト3.42
+  # root "static_pages#home" # リスト3.42
+
 
 end
