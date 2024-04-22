96cc831305b28f40eaf6e7abc74ceb9806cd0fc3
FUMIGear <iso.fumi429@gmail.com>
2024-04-22 18:54:56 +0900

    演習5.3.2が終了しました。

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/about' # リスト3.19
-  get  "static_pages/contact" # 演習3.4.3
-  # root "application#hello" # リスト3.5：デプロイするため
-  root "static_pages#home" # リスト3.42
+  # get 'static_pages/home'
+  # get 'static_pages/help'
+  # get 'static_pages/about' # リスト3.19
+  # get  "static_pages/contact" # 演習3.4.3
+  # # root "application#hello" # リスト3.5：デプロイするため
+  # root "static_pages#home" # リスト3.42
 
-
+# リスト 5.26:静的なページのルーティング一覧
+  root "static_pages#home"
+  get  "/help",    to: "static_pages#help"
+  get  "/about",   to: "static_pages#about"
+  get  "/contact", to: "static_pages#contact"
 end
