8692404e7e764c05760d8522cb33030dae9598d0
FUMIGear <iso.fumi429@gmail.com>
2024-05-08 18:58:47 +0900

    6.3.2が終わったため、コミットします。

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,16 @@
 class User < ApplicationRecord
+  # before_save { self.email = email.downcase }
+  before_save { email.downcase! }
+  #リスト6.9name属性の存在性を検証する
+  validates :name, presence: true
+  # リスト 6.12:email属性の存在性を検証する
+  validates :email, presence: true
+  # リスト6.16：name属性とemail属性に長さの検証を追加する
+  validates :name,  presence: true, length: { maximum: 50 }
+  # VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i #リスト6.21
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i # リスト6.23
+  validates :email, presence: true, length: { maximum: 255 },
+  format: { with: VALID_EMAIL_REGEX }, #リスト6.21
+  # uniqueness: { case_sensitive: false } #リスト6.27
+  uniqueness: true
 end
