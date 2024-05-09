17ae9d0f2f1fbb8ce5ba8d5054c1a10181ad90a3
FUMIGear <iso.fumi429@gmail.com>
2024-05-09 18:36:21 +0900

    Add build script

---
+++ bin/render-build.sh
@@ -0,0 +1,8 @@
+# リスト6.45：ビルドスクリプトを作成する
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+bundle install
+bundle exec rails assets:precompile
+bundle exec rails assets:clean
+bundle exec rails db:migrate
