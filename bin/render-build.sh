034edcfca697e2fa05f826d9c660126d56a4e021
FUMIGear <iso.fumi429@gmail.com>
2024-07-04 12:47:54 +0900

    10.4章が終わり、ユーザ削除のテストを追加しました。

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,3 +1,4 @@
+# リスト 10.65:ビルドスクリプトにdb:migrate:resetとdb:seedを追加する
 # リスト6.45：ビルドスクリプトを作成する
 #!/usr/bin/env bash
 # exit on error
@@ -5,4 +6,6 @@ set -o errexit
 bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
-bundle exec rails db:migrate
+# bundle exec rails db:migrate
+DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rails db:migrate:reset
+bundle exec rails db:seed
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -5,15 +5,18 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   def setup
     @user       = users(:michael)
     @other_user = users(:archer)
+    @admin      = users(:michael) # リスト10.63
+    @non_admin  = users(:lana) # リスト10.63
   end
 
+  # singnupページが開けた確認するテスト
   test "should get new" do
     # get users_new_url
     get signup_path
     assert_response :success
   end
 
-  # リスト 10.35:indexアクションのリダイレクトをテストする red
+  # リスト 10.35:indexアクションのリダイレクトをテストする
   test "should redirect index when not logged in" do
     get users_path
     assert_redirected_to login_url
@@ -35,5 +38,51 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
                                             admin: true} }
                                             # debugger
     assert_not @other_user.admin?
+  end
+
+  # リスト 10.62:管理者権限の制御をアクションレベルでテストする green
+  # ログアウト状態でdeleteを実行→ログイン画面に移るか。
+  test "should redirect destroy when not logged in" do
+    assert_no_difference 'User.count' do
+      delete user_path(@user)
+    end
+    assert_response :see_other
+    assert_redirected_to login_url
+  end
+  # 管理者じゃないアカウントがdeleteを実行→ホーム画面に戻るか
+  test "should redirect destroy when logged in as a non-admin" do
+    log_in_as(@other_user)
+    assert_no_difference 'User.count' do
+      delete user_path(@user)
+    end
+    assert_response :see_other
+    assert_redirected_to root_url
+  end
+
+  # リスト 10.63:削除リンクとユーザー削除に対する統合テスト green
+  test "index as admin including pagination and delete links" do
+    log_in_as(@admin)
+    get users_path
+    assert_template 'users/index'
+    assert_select 'div.pagination'
+    first_page_of_users = User.paginate(page: 1)
+    first_page_of_users.each do |user|
+      assert_select 'a[href=?]', user_path(user), text: user.name # ユーザ名の確認
+      unless user == @admin
+        assert_select 'a[href=?]', user_path(user), text: 'delete' # delete表示を確認
+      end
+    end
+    # deleteアクションが成功するか確認
+    assert_difference 'User.count', -1 do
+      delete user_path(@non_admin)
+      assert_response :see_other
+      assert_redirected_to users_url
+    end
+  end
+  # 管理者でないユーザがログインした場合、deleteという表示がないことを確認
+  test "index as non-admin" do
+    log_in_as(@non_admin)
+    get users_path
+    assert_select 'a', text: 'delete', count: 0
   end
 end
